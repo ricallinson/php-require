17fab89cf3bfbc200cc70906083a0ec4f556c083
Ric Allinson <capecodehq@yahoo.co.uk>
2013-07-02 21:45:13 -0700

    First commit

---
+++ examples/index.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+	For Quick debuging.
+*/
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+header('Content-type: text/plain');
+
+/*
+    Require the "php-require" module loader.
+*/
+
+require("../lib/module.php");
+
+/*
+    $require modules.
+*/
+
+$text = $require("text");
+$math = $require("math");
+
+$text["print"]("Filename: " . $__filename);
+$text["print"]("Dirname: " . $__dirname);
+$text["print"]("Sum: " . $math["sum"](1, 2));
+$text["print"]("Fib: " . $math["fib"](8));
+$text["print"]("Span: " . $text["html"]["span"]("span"));
+$text["print"]("Bold: " . $text["html"]["bold"]("bold"));
